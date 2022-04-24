4a1cfa261ebc0feed8ca1846e3fe46b2e1229ad7
Martin <nitromagix@gmail.com>
2022-04-24 11:37:55 -0700

    finished assignment

--- public/app.js
+++ public/app.js
@@ -3,7 +3,21 @@
 
 
 // get user's time
+const userTime = () => {
+   const now = new Date();
+   return now.getHours();
+}
 
+const getMealTime = () => {
+   const tod = userTime();
+   return tod > 20
+      ? 'latenight snack'
+      : tod > 16
+         ? 'dinner'
+         : tod > 11
+            ? 'lunch'
+            : 'breakfast'
+}
 
 // helper functions
 // check time of day
@@ -12,8 +26,42 @@
 // build ads
 // build ad 1
 
+const buildAd1 = () => {
+   const mealTime = getMealTime();
+   let content = document.querySelector('.ad1');
+   let inner = document.createElement('p');
+   inner.innerHTML = `We've got the best <span>${mealTime}</span> in town.`;
+   content.append(inner);
 
-// build ad 2
+}
+
+
+const getCoords = async() => {
+   const pos = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+   });
+   return [pos.coords.latitude, pos.coords.longitude]
+}
+
+// console.log(getCoords());
+
+const buildAd2 = (coordinates) => {
+   const coords = coordinates
+   const href = `https://www.google.com/maps/search/coffee/@${coords[0]},${coords[1]},15z`
+   let content = document.querySelector('.ad2')
+   let inner = document.createElement('p')
+   inner.innerHTML = `It's time to try our coffee! <span><a href="${href}" target="_blank">We're this close!</a></span>`
+   content.append(inner)
+}
+
+// console.log(buildAd2(getCoords()))
+
+window.onload = async () => {
+   buildAd1();
+   const coords = await getCoords();
+   buildAd2(coords);
+}
+
 
 
 // event listeners
